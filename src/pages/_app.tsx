1fdbc92d8df6f662639ca689add564b7a78b5f5e
marcos <marcos@grana.capital>
2021-10-08 18:10:22 -0300

    initial commit :tada:

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+//carregado toda vez
+
+import "../styles/global.scss";
+
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
